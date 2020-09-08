992740e631ab50f8fdf30419523f6f9e31a7cc6b
Anagha6045 <anaghapaittattu@gmail.com>
2020-09-08 12:43:48 +0530

    My first change

---
+++ mvcforgit/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace mvcforgit
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ mvcforgit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mvcforgit.MvcApplication" Language="C#" %>
